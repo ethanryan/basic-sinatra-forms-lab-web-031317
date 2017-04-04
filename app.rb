3b4b58640e6efbc27c26a86888658a4d20f2ce87
ethanryan <ethanryan@gmail.com>
2017-04-04 17:39:41 -0400

    done

--- app.rb
+++ app.rb
@@ -2,5 +2,24 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :index
+  end
+
+# Create a route that responds to a POST request at /team.
+
+#gets new team info from user input (from newteam.erb)
+  get '/newteam' do
+    erb :newteam
+  end
+
+# Have the form send a POST request to this route.
+# Upon submission, pass the submitted data to
+# the team.erb template.
+
+#posts user input to /team page (to team.erb)
+  post '/team' do
+    erb :team
+  end
 
 end
